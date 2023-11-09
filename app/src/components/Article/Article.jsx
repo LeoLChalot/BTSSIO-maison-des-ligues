ece0a19147b24e39541615885edee2a527e615b3
LeoLChalot <77191750+LeoLChalot@users.noreply.github.com>
2023-11-09 17:42:30 +0100

    Design Card product

--- app/src/components/Article/Article.jsx
+++ app/src/components/Article/Article.jsx
@@ -3,13 +3,40 @@ import './Article.css'
 
 const Produit = ({ nom, photo, description, prix, quantite }) => {
   return (
-    <div className="produit">
-      <p>Nom : {nom}</p>
-      <p>Path poto : {photo}</p>
-      <p>Description : {description}</p>
-      <p>Prix : {prix}</p>
-      <p>Quantité : {quantite}</p>
-    </div>
+    <>
+      <div className="produit">
+        <p>Nom : {nom}</p>
+        <p>Path photo : {photo}</p>
+        <p>Description : {description}</p>
+        <p>Prix : {prix}</p>
+        <p>Quantité : {quantite}</p>
+      </div>
+
+      <div class="card">
+        <div class="card-image"></div>
+        <div class="card-title">
+          <h2>Headphone</h2>
+        </div>
+        <div class="card-detail">
+          <h3>
+            in | <span class="date">Eletronics</span>
+          </h3>
+        </div>
+        <div class="card-text">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae
+            temporibus omnis illum maxime quod deserunt eligendi dolor
+          </p>
+        </div>
+
+        <div class="card-product-price">
+          <p>
+            $<span>3500</span>
+          </p>
+          <button type="button">Buy Now</button>
+        </div>
+      </div>
+    </>
   )
 }
 
